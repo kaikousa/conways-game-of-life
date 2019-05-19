ceaac07f4bafc74c468d9be92de5265742b28d3d
Kai Kousa <kai.kousa@gmail.com>
2019-05-19 20:28:49 +0300

    Extract game logic to a separate module.

---
+++ src/game.js
@@ -0,0 +1,100 @@
+import filter from 'lodash/filter'
+
+export function buildGrid (maxX, maxY) {
+  var grid = {
+    x: maxX,
+    y: maxY,
+    rows: []
+  }
+
+  for (var x = 0; x < grid.x; x++) {
+    grid.rows[x] = []
+    for (var y = 0; y < grid.y; y++) {
+      grid.rows[x][y] = {
+        row: x,
+        cell: y,
+        alive: false
+      }
+    }
+  }
+
+  return grid
+}
+
+// Cycle through the grid and resolve the next generation of cells.
+// Returns an array of state transitions to be applied.
+export function cycle (grid) {
+  var transitions = []
+
+  // Sweep through the grid and determine next states for each cell
+  for (var y = 0; y < grid.rows.length; y++) {
+    var row = grid.rows[y]
+    for (var x = 0; x < row.length; x++) {
+      var cell = row[x]
+      var alive = liveNeighbours(grid, cell)
+
+      if (cell.alive) {
+        // dies of underpopulation or overpopulation
+        if (alive.length < 2 || alive.length > 3) {
+          transitions.push({
+            cell: cell,
+            alive: false
+          })
+        }
+      } else {
+        // becomes alive from reproduction
+        if (alive.length === 3) {
+          transitions.push({
+            cell: cell,
+            alive: true
+          })
+        }
+      }
+    }
+  }
+
+  return transitions
+}
+
+// Returns the living neighbours of a given cell.
+function liveNeighbours (grid, cell) {
+  return filterDead(
+    grid,
+    neighbours(grid, cell)
+  )
+}
+
+function neighbours (grid, cell) {
+  var neighbours = [
+    createNeighbour(cell, -1, 0), // top
+    createNeighbour(cell, 1, 0), // bottom
+    createNeighbour(cell, 0, -1), // left
+    createNeighbour(cell, 0, 1), // right
+    createNeighbour(cell, -1, -1), // topLeft
+    createNeighbour(cell, -1, 1), // topRight
+    createNeighbour(cell, 1, -1), // bottomLeft
+    createNeighbour(cell, 1, 1) // bottomRight
+  ]
+  return filterOutOfBounds(grid, neighbours)
+}
+
+function createNeighbour (cell, rowModifier, cellModifier) {
+  return {
+    row: cell.row + rowModifier,
+    cell: cell.cell + cellModifier
+  }
+}
+
+// Filter neighbours that are out of bounds of the grid dimensions
+function filterOutOfBounds (grid, neighbours) {
+  return filter(neighbours, (o) => {
+    return o.cell >= 0 && o.row >= 0 && o.row < grid.y && o.cell < grid.x
+  })
+}
+
+function filterDead (grid, neighbours) {
+  return filter(neighbours, (o) => {
+    var neighbour = grid.rows[o.row][o.cell]
+    return neighbour.alive
+  })
+}
--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import filter from 'lodash/filter'
 import clone from 'lodash/clone'
 import forEach from 'lodash/forEach'
+import * as game from '@/game'
 
 Vue.use(Vuex)
 
@@ -26,22 +26,7 @@ export default new Vuex.Store({
   },
   actions: {
     buildGrid (context) {
-      var grid = {
-        x: 20,
-        y: 20,
-        rows: []
-      }
-
-      for (var x = 0; x < grid.x; x++) {
-        grid.rows[x] = []
-        for (var y = 0; y < grid.y; y++) {
-          grid.rows[x][y] = {
-            row: x,
-            cell: y,
-            alive: false
-          }
-        }
-      }
+      var grid = game.buildGrid(20, 20)
 
       context.commit('setGrid', grid)
     },
@@ -49,34 +34,7 @@ export default new Vuex.Store({
     cycle (context) {
       var start = new Date()
 
-      var transitions = []
-
-      // Sweep through the grid and determine next states for each cell
-      for (var y = 0; y < context.state.grid.rows.length; y++) {
-        var row = context.state.grid.rows[y]
-        for (var x = 0; x < row.length; x++) {
-          var cell = row[x]
-          var alive = liveNeighbours(context.state.grid, cell)
-
-          if (cell.alive) {
-            if (alive.length < 2 || alive.length > 3) {
-              // dies of underpopulation or overpopulation
-              transitions.push({
-                cell: cell,
-                alive: false
-              })
-            }
-          } else {
-            if (alive.length === 3) {
-              // becomes alive from reproduction
-              transitions.push({
-                cell: cell,
-                alive: true
-              })
-            }
-          }
-        }
-      }
+      var transitions = game.cycle(context.state.grid)
 
       // Transition state between life-death
       forEach(transitions, (t) => {
@@ -91,49 +49,3 @@ export default new Vuex.Store({
     }
   }
 })
-
-function createNeighbour (cell, rowModifier, cellModifier) {
-  return {
-    row: cell.row + rowModifier,
-    cell: cell.cell + cellModifier
-  }
-}
-
-// Filter neighbours that are out of bounds
-function validateNeighbours (grid, neighbours) {
-  return filter(neighbours, (o) => {
-    return o.cell >= 0 && o.row >= 0 && o.row < grid.y && o.cell < grid.x
-  })
-}
-
-function filterDead (grid, neighbours) {
-  // TODO: there is one undefined for some reason
-  if (neighbours === undefined) {
-    return []
-  }
-  return filter(neighbours, (o) => {
-    var neighbour = grid.rows[o.row][o.cell]
-    return neighbour.alive
-  })
-}
-
-function liveNeighbours (grid, cell) {
-  return filterDead(
-    grid,
-    neighbours(grid, cell)
-  )
-}
-
-function neighbours (grid, cell) {
-  var neighbours = [
-    createNeighbour(cell, -1, 0), // top
-    createNeighbour(cell, 1, 0), // bottom
-    createNeighbour(cell, 0, -1), // left
-    createNeighbour(cell, 0, 1), // right
-    createNeighbour(cell, -1, -1), // topLeft
-    createNeighbour(cell, -1, 1), // topRight
-    createNeighbour(cell, 1, -1), // bottomLeft
-    createNeighbour(cell, 1, 1) // bottomRight
-  ]
-  return validateNeighbours(grid, neighbours)
-}
